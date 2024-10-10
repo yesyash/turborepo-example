7a69a0a0d1f451c8ad517ae65f319c6b08c9ba80
yash raj <yashraj0207@outlook.com>
2024-10-11 03:21:49 +0530

    update button to add size prop

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,15 +4,16 @@ import { ReactNode } from "react";
 import { cn } from '@repo/utils/cn'
 
 type ButtonProps = {
+  size: "sm" | "md"
   children: ReactNode;
   className?: string;
   onClick?: () => void
 }
 
-export const Button = ({ children, className, onClick }: ButtonProps) => {
+export const Button = ({ size, children, className, onClick }: ButtonProps) => {
   return (
     <button
-      className={cn("bg-blue-600 text-white px-4 py-2 text-base", className)}
+      className={cn("bg-blue-600 text-white px-4 py-2 text-base", size === 'sm' ? "text-sm" : "text-base", className)}
       onClick={onClick}
     >
       {children}
