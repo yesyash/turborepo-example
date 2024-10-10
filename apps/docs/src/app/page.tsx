c9ef63a51ba42bb9275bbd93d7ee2e10ab6e6ab1
yash raj <yashraj0207@outlook.com>
2024-10-11 03:05:45 +0530

    udpate docs landing page

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -5,8 +5,8 @@ import { Button } from "@repo/ui/button";
 export default function Home() {
   return (
     <div className="p-6">
-      <div className="text-4xl font-bold pb-2 text-magenta">Document app</div>
-      <Button className="text-sm bg-yellow-600" onClick={() => window.alert("welcome to document app")}>Alert</Button>
+      <div className="text-4xl font-bold pb-2 text-magenta">Docs</div>
+      <Button className="text-sm bg-yellow-600" onClick={() => window.alert("welcome to docs")}>Alert</Button>
     </div>
   );
 }
