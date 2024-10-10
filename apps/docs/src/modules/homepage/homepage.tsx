e1c9646d6161ed42c0091620027cc738643d35d3
Yash Raj <56453897+yesyash@users.noreply.github.com>
2024-10-11 03:18:49 +0530

    create a homepage module (#2)

---
+++ apps/docs/src/modules/homepage/homepage.tsx
@@ -0,0 +1,10 @@
+import { AlertButton } from "./alert-button";
+
+export function Homepage() {
+  return (
+    <div className="p-6">
+      <div className="text-4xl font-bold pb-2 text-magenta">Docs</div>
+      <AlertButton />
+    </div>
+  );
+}
